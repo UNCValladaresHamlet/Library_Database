d76e2a91b14785d379a52e01819697e2914adc24
Hamlet Valladares <sigorahv@gmail.com>
2023-05-25 16:43:23 -0400

    added data to the books table

--- CommandsDB.sql
+++ CommandsDB.sql
@@ -1,4 +1,4 @@
--- Creates new rows containing data in all named columns for the Authors table --
+-- Creates new rows containing data in all named columns for the 'Authors' table --
 
 INSERT INTO Authors (AuthorFirstName, AuthorLastName, AuthorNationality) values ('Sofia', 'Smith', 'Canada');
 INSERT INTO Authors (AuthorFirstName, AuthorLastName, AuthorNationality) values ('Maria', 'Brown', 'Brazil');
@@ -20,6 +20,42 @@ INSERT INTO Authors (AuthorFirstName, AuthorLastName, AuthorNationality) values 
 INSERT INTO Authors (AuthorFirstName, AuthorLastName, AuthorNationality) values ('David', 'Wright', 'Canada');
 INSERT INTO Authors (AuthorFirstName, AuthorLastName, AuthorNationality) values ('Thomas', 'Scott', 'Canada');
 INSERT INTO Authors (AuthorFirstName, AuthorLastName, AuthorNationality) values ('Helena', 'Adams', 'Canada');
-INSERT INTO Authors (AuthorFirstName, AuthorLastName, AuthorNationality) values ('Sofia', '	Carter', 'USA');
+INSERT INTO Authors (AuthorFirstName, AuthorLastName, AuthorNationality) values ('Sofia', 'Carter', 'USA');
 INSERT INTO Authors (AuthorFirstName, AuthorLastName, AuthorNationality) values ('Liam', 'Parker', 'Canada');
 INSERT INTO Authors (AuthorFirstName, AuthorLastName, AuthorNationality) values ('Emily', 'Murphy', 'USA');
+
+
+-- Creates new rows containing data in all named columns for the 'Books' table --
+
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Build your database system', 1, 'Science');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('The red wall', 2 , 'Fiction');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('The perfect match', 3 , 'Fiction');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Digital Logic', 4 , 'Science');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('How to be a great lawyer', 5 , 'Law');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Manage successful negotiations', 6 , 'Society');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Pollution today', 7 , 'Science');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('A gray park', 2 , 'Fiction');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('How to be rich in one year', 8 , 'Humor');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Their bright fate', 9 , 'Fiction');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Black lines', 10 , 'Fiction');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('History of theater', 11 , 'Literature');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Electrical transformers', 12 , 'Science');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Build your big data system', 1 , 'Science');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Right and left', 13 , 'Children');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Programming using Python', 1 , 'Science');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Computer networks', 14 , 'Science');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Performance evaluation', 15 , 'Science');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Daily exercise', 16 , 'Well being');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('The silver uniform', 17 , 'Fiction');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Industrial revolution', 18 , 'History');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Green nature', 19 , 'Well being');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Perfect football', 20 , 'Well being');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('The chocolate love', 21 , 'Humor');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Director and leader', 22 , 'Society');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Play football every week', 20 , 'Well being');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Maya the bee', 13 , 'Children');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Perfect rugby', 20  , 'Well being');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('The end', 23  , 'Fiction');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Computer security', 1  , 'Science');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Participate', 22  , 'Society');
+INSERT INTO Books (BookTitle, AuthorID, Genre) values ('Positive figures', 3  , 'Fiction');
