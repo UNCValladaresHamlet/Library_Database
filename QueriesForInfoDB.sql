807a6590e30d1b175eda96560c6441a2575d6fe9
Hamlet Valladares <sigorahv@gmail.com>
2023-05-29 18:56:07 -0400

    Added better comments, need to finish queries

--- QueriesForInfoDB.sql
+++ QueriesForInfoDB.sql
@@ -1,20 +1,37 @@
--- Write the following queries to retrieve the information detailed below. --
-
 -- 1. Display all contents of the Clients table --
 
 -- This command below selects all (The asterisk or star symbol ( * ) means all columns.) from Clients table.
--- I added a auto increment in the ClientsId column which lists the table data in ascending order.
+-- I added a auto increment in the ClientsID column which lists the table data in ascending order.
+-- I attached MySQL syntax as well:
+-- SELECT * ...
+-- FROM table_name;
+
 	SELECT * 
     FROM Clients;
 
 -- 2. First names, last names, ages and occupations of all clients --
 
--- This command selects these specific columns: ClientFirstName, ClientLastName, ClientDoB, Occupation from the Clients table
--- I used a YEAR() functon which returns the year part for a given date and used a NOW() function to return the current date and time. I used that date data to subtract the current year from the Client DoB year which results in the current age of the client. Used the AS command to rename the column 'ClientDoB' as 'Age'.
+-- This command selects these specific columns: 'ClientFirstName, ClientLastName, ClientDoB, Occupation' from the Clients table
+-- I used a YEAR() functon which returns the year part for a given date and used a NOW() function to return the current date and time. I used that date data to subtract the current year from the Client DoB year which results in the current age of the client. Used the AS command to rename the column 'ClientDoB' as the 'Age' column.
+-- I attached MySQL syntax as well:
+-- SELECT column1, column2, column 3 AS alias_name, column 4
+-- FROM table_name;
+
      SELECT ClientFirstName, ClientLastName, (YEAR(NOW())-ClientDoB) AS Age, Occupation
     FROM Clients;
 
 -- 3. First and last names of clients that borrowed books in March 2018 --
+
+-- This commands selects these specific columns: ClientFirstName and ClientLastName from the Clients table.
+-- I used a INNER JOIN to selects records that help me find the matching values from the Clients table and the selected Borrowers table. I specified which tables and columns I wanted to access using the ON keyword. I used the 'ClientsID' column that the tables both share to finish the INNER JOIN. I used the WHERE clause to filter the records in order to extract only those records in the 'BorrowDate' column that are between the March 1st, 2018 and March 31st, 2018. I used a GROUP BY statement to group the result-set by the 'ClientsID' column. I originally added a auto increment in the ClientsID column which lists the table data in ascending order.
+
+-- SELECT column_name(s)
+-- FROM table1
+-- INNER JOIN table2
+-- ON table1.column_name = table2.column_name;
+-- WHERE condition
+-- GROUP BY column_name(s)
+
     SELECT ClientFirstName,ClientLastName
     FROM Clients
     INNER JOIN Borrowers
@@ -41,7 +58,7 @@
     ON Books.BookID = Borrowers.BookID
     WHERE BorrowDate Between '2015-01-01' AND '2017-01-01'
     GROUP BY AuthorNationality
-    ORDER BY COUNT(Borrowers.BookID) ASC LIMIT 5;
+    ORDER BY COUNT(Borrowers.BookID) LIMIT 5;
 
 -- The book that was most borrowed during the years 2015-2017 --
 
