4caaf62696b0b77ab9ca616a607799d15bc1a1d5
Hamlet Valladares <sigorahv@gmail.com>
2023-06-01 21:31:38 -0400

    Simple push

--- QueriesForInfoDB.sql
+++ QueriesForInfoDB.sql
@@ -2,7 +2,7 @@
 
 -- This query selects all (The asterisk or star symbol ( * ) means all columns) from 'Clients' table. I added a auto increment in the 'ClientsID' column which lists the table data in ascending order.
 -- I attached MySQL syntax as well:
--- SELECT select_list ...
+-- SELECT select_list
 -- FROM table_name;
 
 	SELECT *
@@ -10,7 +10,7 @@
 
 -- 2. First names, last names, ages and occupations of all clients --
 
--- This query selects these specific columns: 'ClientFirstName, ClientLastName, ClientDoB, Occupation' from the 'Clients' table to list in the result-set. I used a YEAR() function which returns the year part for a given date and used a NOW() function to return the current date and time. I used that date information to subtract the current year from the Client DOB year which results in the current age of all the clients listed. Used the AS command to rename the column 'ClientDOB' as the 'Age' column.
+-- This query selects these specific columns: 'ClientFirstName, ClientLastName, ClientDoB, Occupation' from the 'Clients' table to list in the result-set. I used a YEAR() function which returns the year component for a given date and used a NOW() function to return the current date and time. I used that date information to subtract the current year from the Client DOB year which results in the current age of all the clients listed. Used the AS command to rename the column 'ClientDOB' as the 'Age' column.
 -- I attached MySQL syntax as well:
 -- SELECT column1, column2, column 3 AS alias_name, column 4
 -- FROM table_name;
@@ -20,7 +20,7 @@
 
 -- 3. First and last names of clients that borrowed books in March 2018 --
 
--- This query selects these specific columns: ClientFirstName and ClientLastName from the 'Clients' table to list in the result-set. I used a INNER JOIN to selects records that help me find the matching values from the 'Clients' table and the selected 'Borrowers' table. I specified which tables and columns I wanted to access using the ON keyword. I used the 'ClientsID' column that the tables both share to finish the INNER JOIN. I used the WHERE clause to filter the records in order to extract only those records in the 'BorrowDate' column that are between the March 1st, 2018 and March 31st, 2018. I used a GROUP BY statement to group the result-set by the 'ClientsID' column. I originally added a auto increment in the 'ClientsID' column which lists the table data in ascending order.
+-- This query selects these specific columns: ClientFirstName and ClientLastName from the 'Clients' table to list in the result-set. I used a INNER JOIN to select records that help me find the matching values from the 'Clients' table and the selected 'Borrowers' table. I specified which tables and columns I wanted to access using the ON keyword. I used the 'ClientsID' column that the tables both share to finish the INNER JOIN. I used the WHERE clause to filter the records in order to extract only those records in the 'BorrowDate' column that are between the March 1st, 2018 and March 31st, 2018. I used a GROUP BY statement to group the result-set by the 'ClientsID' column. I originally added a auto increment in the 'ClientsID' column which lists the table data in ascending order.
 -- I attached MySQL syntax as well:
 -- SELECT select_list
 -- FROM table1
@@ -38,7 +38,7 @@
 
 -- 4. First and last names of the top 5 authors clients borrowed in 2017 --
 
--- This command selects these specific columns: 'AuthorFirstName', 'AuthorLastName' from the 'Authors' table to list in the result-set. I used a INNER JOIN to selects records that help me find the matching values from the 'Authors' table and the selected 'Books' table. I specified which tables and columns I wanted to access using the ON keyword. I used the 'AuthorsID' column that the tables both share to finish the INNER JOIN. I used a second INNER JOIN to find the matching values from the 'Books' table and the selected 'Borrowers' table. I used the 'BooksID' column that the tables both share to finish the second INNER JOIN. I used the WHERE clause to filter the records in order to extract only those records in the 'BorrowDate' column that are between the January 1st, 2017 and December 31st, 2017. I used a GROUP BY statement to group the result-set by the 'BookID' column. I then used the ORDER BY keyword and the the COUNT() function to count how many times the number of books in the result-set was borrowed in the "Borrowed" table. I sorted the result-set in descending order (Included the DESC keyword) and used a LIMIT clause to specify the number of records to return, I used 5 to result in the top 5 authors.
+-- This command selects these specific columns: 'AuthorFirstName', 'AuthorLastName' from the 'Authors' table to list in the result-set. I used a INNER JOIN to select records that help me find the matching values from the 'Authors' table and the selected 'Books' table. I specified which tables and columns I wanted to access using the ON keyword. I used the 'AuthorsID' column that the tables both share to finish the INNER JOIN. I used a second INNER JOIN to find the matching values from the 'Books' table and the selected 'Borrowers' table. I used the 'BooksID' column that the tables both share to finish the second INNER JOIN. I used the WHERE clause to filter the records in order to extract only those records in the 'BorrowDate' column that are between the January 1st, 2017 and December 31st, 2017. I used a GROUP BY statement to group the result-set by the 'BookID' column. I then used the ORDER BY keyword and the the COUNT() function to count how many times the number of books in the result-set was borrowed in the "Borrowed" table. I sorted the result-set in descending order (Included the DESC keyword) and used a LIMIT clause to specify the number of records to return, I used 5 to result in the top 5 authors.
 -- I attached MySQL syntax as well:
 -- SELECT column_name(s)
 -- FROM table1
@@ -62,7 +62,7 @@
 
 -- 5. Nationalities of the least 5 authors that clients borrowed during the years 2015-2017 --
 
--- This query selects this specific column: 'AuthorNationality' from the 'Authors' table to list in the result-set. I used a INNER JOIN to selects records that help me find the matching values from the 'Authors' table and the selected 'Books' table. I specified which tables and columns I wanted to access using the ON keyword. I used the 'AuthorsID' column that the tables both share to finish the INNER JOIN. I used a second INNER JOIN to find the matching values from the 'Books' table and the selected 'Borrowers' table. I used the 'BooksID' column that the tables both share to finish the second INNER JOIN. I used the WHERE clause to filter the records in order to extract only those records in the 'BorrowDate' column that are between the January 1st, 2015 and December 31st, 2017. I used a GROUP BY statement to group the result-set by the 'AuthorNationality' column. I then used the ORDER BY keyword and the The COUNT() function to count how many times the number of books in the result-set was borrowed in the "Borrowed" table. I originally added a auto increment in the Book ID column which lists the table data in ascending order. I sorted the result-set and used a LIMIT clause to specify the number of records to return, I used 5 to list the least 5 nationalities in the result-set.
+-- This query selects this specific column: 'AuthorNationality' from the 'Authors' table to list in the result-set. I used a INNER JOIN to select records that help me find the matching values from the 'Authors' table and the selected 'Books' table. I specified which tables and columns I wanted to access using the ON keyword. I used the 'AuthorsID' column that the tables both share to finish the INNER JOIN. I used a second INNER JOIN to find the matching values from the 'Books' table and the selected 'Borrowers' table. I used the 'BooksID' column that the tables both share to finish the second INNER JOIN. I used the WHERE clause to filter the records in order to extract only those records in the 'BorrowDate' column that are between the January 1st, 2015 and December 31st, 2017. I used a GROUP BY statement to group the result-set by the 'AuthorNationality' column. I then used the ORDER BY keyword and the The COUNT() function to count how many times the number of books in the result-set was borrowed in the "Borrowed" table. I originally added a auto increment in the Book ID column which lists the table data in ascending order. I sorted the result-set and used a LIMIT clause to specify the number of records to return, I used 5 to list the least 5 nationalities in the result-set.
 -- I attached MySQL syntax as well:
 -- SELECT select_list
 -- FROM table1
@@ -86,7 +86,7 @@
 
 -- 6. The book that was most borrowed during the years 2015-2017 --
 
--- This query selects this specific column: 'AuthorNationality' from the 'Books' table to list in the result-set. I used a INNER JOIN to selects records that help me find the matching values from the 'Books' table and the selected 'Borrowers' table. I specified which tables and columns I wanted to access using the ON keyword. I used the 'BookID' column that the tables both share to finish the INNER JOIN. I used the WHERE clause to filter the records in order to extract only those records in the 'BorrowDate' column that are between the January 1st, 2015 and December 31st, 2017. I used a GROUP BY statement to group the result-set by the 'BookID' column. I then used the ORDER BY keyword and the The COUNT() function to count how many times the number of books in the result-set was borrowed in the "Borrowed" table. I sorted the result-set in descending order (Included the DESC keyword) and used a LIMIT clause to specify the number of records to return, I used 1 to result in the book that was the most borrowed thoughout 2015-2017.
+-- This query selects this specific column: 'AuthorNationality' from the 'Books' table to list in the result-set. I used a INNER JOIN to select records that help me find the matching values from the 'Books' table and the selected 'Borrowers' table. I specified which tables and columns I wanted to access using the ON keyword. I used the 'BookID' column that the tables both share to finish the INNER JOIN. I used the WHERE clause to filter the records in order to extract only those records in the 'BorrowDate' column that are between the January 1st, 2015 and December 31st, 2017. I used a GROUP BY statement to group the result-set by the 'BookID' column. I then used the ORDER BY keyword and the The COUNT() function to count how many times the number of books in the result-set was borrowed in the "Borrowed" table. I sorted the result-set in descending order (Included the DESC keyword) and used a LIMIT clause to specify the number of records to return, I used 1 to result in the book that was the most borrowed thoughout 2015-2017.
 -- I attached MySQL syntax as well:
 -- SELECT select_list
 -- FROM table1
@@ -107,7 +107,7 @@ ORDER BY COUNT(Borrowers.BookID) DESC LIMIT 1;
 
 -- 7. Top borrowed genres for client born in years 1970-1980 --
 
--- This query selects this specific column: 'Genre' column from the 'Books' table to list in the result-set. I used a INNER JOIN to selects records that help me find the matching values from the 'Books' table and the selected 'Borrowers' table. I specified which tables and columns I wanted to access using the ON keyword. I used the 'BookID' column that the tables both share to finish the INNER JOIN. I used a second INNER JOIN to find the matching values from the 'Borrowers' table and the selected 'Clients' table. I used the 'ClientID' column that the tables both share to finish the second INNER JOIN. I used the WHERE clause to filter the records in order to extract only those records in the 'ClientDOB' column that are between the year 1970 and 1980. I used a GROUP BY statement to group the result-set by the 'Genre' column, I then used the ORDER BY keyword to list the result set of the top borrowed genres in Ascending order.
+-- This query selects this specific column: 'Genre' column from the 'Books' table to list in the result-set. I used a INNER JOIN to select records that help me find the matching values from the 'Books' table and the selected 'Borrowers' table. I specified which tables and columns I wanted to access using the ON keyword. I used the 'BookID' column that the tables both share to finish the INNER JOIN. I used a second INNER JOIN to find the matching values from the 'Borrowers' table and the selected 'Clients' table. I used the 'ClientID' column that the tables both share to finish the second INNER JOIN. I used the WHERE clause to filter the records in order to extract only those records in the 'ClientDOB' column that are between the year 1970 and 1980. I used a GROUP BY statement to group the result-set by the 'Genre' column, I then used the ORDER BY keyword to list the result set of the top borrowed genres in Ascending order.
 -- I attached MySQL syntax as well:
 -- SELECT select_list
 -- FROM table1
@@ -132,7 +132,7 @@ ORDER BY Books.Genre;
 
 -- 8. Top 5 occupations that borrowed the most in 2016 --
 
--- This query selects this specific column: 'Occupation' from the 'Clients' table to list in the result-set. I used a INNER JOIN to selects records that help me find the matching values from the 'Clients' table and the selected 'Borrowers' table. I specified which tables and columns I wanted to access using the ON keyword. I used the 'ClientID' column that the tables both share to finish the INNER JOIN. I used the WHERE clause to filter the records in order to extract only those records in the 'BorrowDate' column. I used a YEAR() function with the variable inside the function being the BorrowDate and I set that equal to the year '2016'. I used a GROUP BY statement to group the result-set by the 'Occupation' column. I then used the ORDER BY keyword and the COUNT() function to count how many times a client borrows a book. I sorted the result-set in descending order (Included the DESC keyword) and used a LIMIT clause to specify the number of records to return, I used 5 to result in the occupations that borrowed the most books thoughout year 2016.
+-- This query selects this specific column: 'Occupation' from the 'Clients' table to list in the result-set. I used a INNER JOIN to select records that help me find the matching values from the 'Clients' table and the selected 'Borrowers' table. I specified which tables and columns I wanted to access using the ON keyword. I used the 'ClientID' column that the tables both share to finish the INNER JOIN. I used the WHERE clause to filter the records in order to extract only those records in the 'BorrowDate' column. I used a YEAR() function with the variable inside the function being the BorrowDate and I set that equal to the year '2016'. I used a GROUP BY statement to group the result-set by the 'Occupation' column. I then used the ORDER BY keyword and the COUNT() function to count how many times a client borrows a book. I sorted the result-set in descending order (Included the DESC keyword) and used a LIMIT clause to specify the number of records to return, I used 5 to result in the occupations that borrowed the most books thoughout year 2016.
 -- I attached MySQL syntax as well:
 -- SELECT select_list
 -- FROM table1
@@ -169,7 +169,7 @@ GROUP BY Clients.Occupation;
 
 -- 10. Create a VIEW and display the titles that were borrowed by at least 20% of clients --
 
--- Creating a view in SQL allows the user to easily find relevant information in large datasets. I created a view with the CREATE VIEW statement and gave it the name 'books borrowed by 20% of the clients' to preserve the importance of this view table. I use SELECT to identify the existing column I wish to pull into the view, which is 'BookTitle' column from the 'Books' table. I used a INNER JOIN to selects records that help me find the matching values from the 'Books' table and the selected 'Borrowers' table. I specified which tables and columns I wanted to access using the ON keyword. I used the 'BookID' column that the tables both share to finish the INNER JOIN. I used a GROUP BY statement to group the result-set by the 'BookTitle' column.
+-- Creating a view in SQL allows the user to easily find relevant information in large datasets. I created a view with the CREATE VIEW statement and gave it the name 'books borrowed by 20% of the clients' to preserve the importance of this view table. I use SELECT to identify the existing column I wish to pull into the view, which is 'BookTitle' column from the 'Books' table. I used a INNER JOIN to selects records that help me find the matching values from the 'Books' table and the selected 'Borrowers' table. I specified which tables and columns I wanted to access using the ON keyword. I used the 'BookID' column that the tables both share to finish the INNER JOIN. I used a GROUP BY statement to group the result-set by the 'BookTitle' column. I then used the HAVING clause to specify filter conditions for a group of rows or aggregates.
 
 -- I attached MySQL syntax as well:
 -- CREATE VIEW view_name AS
@@ -188,11 +188,14 @@ ON Books.BookID = Borrowers.BookID
 GROUP BY BookTitle
 HAVING COUNT(Borrowers.ClientID) >= (COUNT(Borrowers.ClientID)*0.2)
 
+-- Query to prove the view table was created
+
 SELECT *
 FROM `books borrowed by 20% of the clients`;
 
 
 -- 11. The top month of borrows in 2017 --
+-- I used the MONTH() function to return the month component of the 'BorrowDate' column from the 'Borrowers' table and used the AS command to rename it to 'Top_Month' column. I then used the COUNT function in combination with the MONTH function to count how many books were borrowed. I then used the YEAR() function return the year component of the 'BorrowDate' column and set that equal to 2017 for search condition in the WHERE clause. I used a GROUP BY statement to group the result-set by the 'Genre' column, I then used the ORDER BY keyword to list the result set of the top borrowed genres in Ascending order.
 
  SELECT MONTH(BorrowDate) AS Top_Month, COUNT(MONTH(BorrowDate)) AS Amount_of_books_borrowed
  FROM Borrowers
